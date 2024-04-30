ce747bc7e57496c71a7717846df229a890335544
ryanhanks-bestow <87093311+ryanhanks-bestow@users.noreply.github.com>
2024-04-30 18:06:50 -0500

    chore: pull latest flakkacode/flakka (#3)
    * chore: pull latest flakkacode/flakka
    
    * ci: config error
    
    * ci: provide deps
    
    * ci: provide deps
    
    * chore: placate pana with something

---
+++ lib/src/flakka_protobuf_build_types.dart
@@ -0,0 +1,7 @@
+/// {@template flakka_protobuf_build_types}
+/// Compiled Dart type for flakka_protobuf_build
+/// {@endtemplate}
+class FlakkaProtobufBuildTypes {
+  /// {@macro flakka_protobuf_build_types}
+  const FlakkaProtobufBuildTypes();
+}
