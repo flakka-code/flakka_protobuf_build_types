b48ca6471794236a4a80b3e9b567c486bcfff38a
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-04-29 21:36:12 -0500

    chore: initial commit

---
+++ test/src/flakka_protobuf_build_types_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flakka_protobuf_build_types/flakka_protobuf_build_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FlakkaProtobufBuildTypes', () {
+    test('can be instantiated', () {
+      expect(FlakkaProtobufBuildTypes(), isNotNull);
+    });
+  });
+}
