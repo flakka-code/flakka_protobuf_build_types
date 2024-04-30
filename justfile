fa93b712fca36a492b1c200149031cfd0063adf4
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-04-30 17:32:02 -0500

    ci: add buf-generate to ci load, refactor, fixup

--- justfile
+++ justfile
@@ -1,5 +1,12 @@
 build:
   @buf generate --include-imports --include-wkt
+  @dart format .
+
+full: clean export_proto build
 
 clean:
   rm -rf lib
+  @rm -rf 'proto/*/'
+
+export_proto:
+  @buf export buf.build/flakkacode/flakka --output proto
--- lib/src/flakka_protobuf_build_types.dart
+++
@@ -1,7 +0,0 @@
-/// {@template flakka_protobuf_build_types}
-/// Compiled Dart type for flakka_protobuf_build
-/// {@endtemplate}
-class FlakkaProtobufBuildTypes {
-  /// {@macro flakka_protobuf_build_types}
-  const FlakkaProtobufBuildTypes();
-}
